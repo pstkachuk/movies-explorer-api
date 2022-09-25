a95ae777e8bf1808f42e2e43fae82c14aa7469cb
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-09-26 00:05:35 +0300

    исправить ошибки валидации схемы user

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -10,7 +10,7 @@ const validateLogin = celebrate({
 
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
