5d9b40ad14792a3321d4e50e87305617f214f585
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-09-25 22:58:56 +0300

    сменить номер порта сервера

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const routes = require('./routes/index');
 const { limiter, dataBase } = require('./utils/config');
 const { messageValidationError } = require('./utils/const');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect(dataBase, {
